ef172c9c4b0557af9286571e7932302f48825d5f
Michał Korczak <michal.k2001@gmail.com>
2024-12-28 04:06:55 +0100

    wip: plugin handler

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,7 +3,16 @@ penpot.ui.open('Pattern Hero', `?theme=${penpot.theme}`, {
   height: 500,
 });
 
-penpot.ui.onMessage(() => {});
+penpot.ui.onMessage<any>((message) => {
+  if (message.type === 'create-pattern') {
+    const { config, name } = message.content;
+
+    if (!config || !name) return;
+    if (penpot.selection.length === 0) return;
+
+    // TODO
+  }
+});
 
 // Update the theme in the iframe
 penpot.on('themechange', (theme) => {
